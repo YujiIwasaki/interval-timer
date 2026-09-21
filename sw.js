b9600e8465aadd69752cff8f8c537fd345385560
YujiIwasaki <48954173+YujiIwasaki@users.noreply.github.com>
2026-09-22 08:02:53 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+// オフラインでも動くように、アプリ本体をiPadに保存しておく
+const CACHE = "interval-timer-v1";
+const CORE = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  // ネットにつながるときは最新版を使い、つながらないときは保存済みの版を使う
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match("./index.html")))
+  );
+});
